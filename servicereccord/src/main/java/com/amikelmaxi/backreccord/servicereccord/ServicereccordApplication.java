cbe4f057529e08ea471eb2376c9b886cd450533f
maxloversist19 <maxloversist19@gmail.com>
2024-04-23 18:36:33 -0300

    Projeto criado

---
+++ servicereccord/src/main/java/com/amikelmaxi/backreccord/servicereccord/ServicereccordApplication.java
@@ -0,0 +1,13 @@
+package com.amikelmaxi.backreccord.servicereccord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicereccordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicereccordApplication.class, args);
+	}
+
+}
---
+++ servicereccord/src/test/java/com/amikelmaxi/backreccord/servicereccord/ServicereccordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amikelmaxi.backreccord.servicereccord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicereccordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
