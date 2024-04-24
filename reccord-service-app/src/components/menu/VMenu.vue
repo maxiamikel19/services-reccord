c77d4ffbbf5089a2b39430b1eb878532c1eec832
maxloversist19 <maxloversist19@gmail.com>
2024-04-24 13:21:44 -0300

    Instalar dependencias de Vue Router e sass loader

--- reccord-service-app/src/components/menu/VMenu.vue
+++ reccord-service-app/src/components/menu/VMenu.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-        <h1>Menu main</h1>
+    <div class="main">
+        <div class="main-title">
+            <h3>Services managment</h3>
+        </div>
+        <div class="btn-group ">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                <li><a class="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+        </div>
+        <div class="notes">
+            <p>App v-001</p>
+        </div>
     </div>
 </template>
 
@@ -10,6 +25,19 @@
     }
 </script>
 
-<style  scoped>
+<style lang="scss" scoped>
+@import '../../styles/global.scss';
+    .main{
+        background: transparent;
+        height: 100%;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+     }
 
+    .main-title{
+            border: solid 1px $color-1;
+        }
 </style>
